3d65a55a8c53c7798c57eec7c7c21c15819df207
numd-74 <74402384+numd-74@users.noreply.github.com>
2022-02-08 15:34:44 +0900

    PUN2とGameManager（雛型）を追加

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameMode
+    {
+        Movie,
+        IconSelect,
+        RoomSelect,
+        InRoom,
+        MainGame,
+        Result
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
